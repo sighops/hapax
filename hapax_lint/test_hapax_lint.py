0d71f22e47ba0cc3a4475322e5cea603e5687fe4
caleb <calebdjones@gmail.com>
2024-07-25 21:26:06 -0400

    liniting, add .gitignore

--- hapax_lint/test_hapax_lint.py
+++ hapax_lint/test_hapax_lint.py
@@ -4,57 +4,56 @@ linter = HapaxInstrumentLinter()
 
 def test_is_in_range_true():
     result = linter.is_in_range(55, 1, 100)
-    assert result == True
-
+    assert result is True
 
 def test_is_in_range_false():
     result = linter.is_in_range(450, 1, 100)
-    assert result == False
+    assert result is False
 
 def test_is_in_range_false_when_string():
     result = linter.is_in_range("cheese", 1, 100)
-    assert result == False
+    assert result is False
 
 def test_depth_is_valid():
     result = linter.depth_is_valid(7)
-    assert result == True
+    assert result is True
     result = linter.depth_is_valid(14)
-    assert result == True
+    assert result is True
     result = linter.depth_is_valid(5)
-    assert result == False
+    assert result is False
 
 def test_lint_NRPN():
     line = "23:55:7 Something"
     result = linter.lint_NRPN(line)
-    assert result == True
+    assert result is True
 
     line = "23:55:7:DEFAULT=122 Something Else"
     result = linter.lint_NRPN(line)
-    assert result == True
+    assert result is True
 
 def test_lint_CC():
     line = "1:DEFAULT=64 Modulation Wheel"
     result = linter.lint_CC(line)
-    assert result == True
+    assert result is True
 
     line = "5 PORTAMENTO TIME"
     result = linter.lint_CC(line)
-    assert result == True
+    assert result is True
 
 def test_lint_PC():
     line = "111:1:0 NAME WITH SPACES"
     result = linter.lint_PC(line)
-    assert result == True
+    assert result is True
 
     line = "114 NAME"
     result = linter.lint_PC(line)
-    assert result == True
+    assert result is True
 
 def test_lint_drumlanes():
     line = "3:45:8:89 DRUMLANE 1"
     result = linter.lint_drumlanes(line)
-    assert result == True
+    assert result is True
 
     line = "7:22:CVG1:122 DRUMLANE 2"
     result = linter.lint_drumlanes(line)
-    assert result == True
+    assert result is True
